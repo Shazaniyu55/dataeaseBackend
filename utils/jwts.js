fc1fa7f1f578e924027fa9b8d4206e7a6e45ae63
shazaniyu55 <shazaniyu@gmail.com>
2026-02-27 10:19:33 +0100

    'server'

--- utils/jwts.js
+++ utils/jwts.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
-console.log("JWT Secret:", JWT_SECRET); // Debugging line to check if JWT_SECRET is loaded correctly
 
 const jwtSign = (payload) => {
   return jwt.sign(payload, JWT_SECRET, {
